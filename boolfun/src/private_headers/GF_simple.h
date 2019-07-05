3909db6d52ee362b094b7c462fa54d3791f23b29
comiam <maxim.bolshim@yandex.ru>
2019-07-05 21:35:15 +0700

    Some fixes in GFSimple

--- boolfun/src/private_headers/GF_simple.h
+++ boolfun/src/private_headers/GF_simple.h
@@ -3,14 +3,9 @@
 
 namespace bf 
 {
-	class GFSimple :public GF
+	class GFSimple: public GF
 	{
-	private:
-		bvect32 field_polynom;
-		bvect32 gen_el;
-		bvect32* num_to_deg;//Массив отображения число - степень попрождающего элемента
-		bvect32* deg_to_num;//Массив отображения степень порождающего элемента - число
-		unsigned char order;
+	protected:
 		virtual bvect32 mod(bvect32);
 		virtual bvect32 get_generating_element();
 	public:
