8dcf2e16fb0105d617980a73c68b7ca8890bcbe9
Igor L <ilip.z@ya.ru>
2019-07-10 11:59:05 +0700

    Clone ttable and polynom to 64bit

--- boolfun/headers/polynom.h
+++ boolfun/headers/polynom.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cmath>
 #include "GF.h"
+#include "ttable.h"
 
 // Max polynom length 2^n (n <= 32)
 // Max var quanity n (n <= 32)
@@ -11,8 +12,9 @@ namespace bf {
 
 
 class ttable;
+class ttable64;
 
-class polynom {
+class polynom { // over field type
 	
 	public:
 		polynom (GF * field);
@@ -25,6 +27,22 @@ class polynom {
 	private:
 		std::vector<unsigned char> _coeff;
 		uint32_t _length;
+		GF * _field;
+};
+
+class polynom64 { // over field type
+	
+	public:
+		polynom64 (GF * field);
+		polynom64::polynom64(const ttable &t, GF * field);
+		void set_coeff (uint64_t index, bvect64 coeff);
+		unsigned char get_coeff (uint64_t index);
+		uint64_t get_length ();
+		GF* get_field();
+			
+	private:
+		std::vector<unsigned char> _coeff;
+		uint64_t _length;
 		GF * _field;
 };
 	
--- boolfun/src/polynom.cpp
+++ boolfun/src/polynom.cpp
@@ -37,5 +37,42 @@ namespace bf {
 	GF * polynom::get_field() {
 		return this->_field;
 	}
+	
+	polynom64::polynom64 (GF * field) {
+		this-_length = power2 ( static_cast<uint64_t>(field->get_order()) );
+		this->_coeff.resize( static_cast<size_t>(this->_length) );
+	}
+
+
+	void polynom64::set_coeff(uint64_t index, bvect64 coeff) {
+		this->_coeff[index] = coeff;
+	}
+
+	unsigned char polynom64::get_coeff(uint64_t index) {
+		return this->_coeff[index];
+	}
+
+	polynom64::polynom64 (const ttable &t, GF * field) {
+		this->_length = t->get_length();
+		this->_coeff.resize( static_cast<size_t>(this->_length) );
+		this->set_coeff(0,t.get_value(0));
+		vector<bvect64> values;
+		const uint64_t tlen = tlen;
+		for (uint64_t i=0; i < tlen; i++)
+			values.push_back( t.get_value(i) )
+			for (uint_64 coeffq = 0; coeffq < this->_length; coeffq++)
+				for (uint64 valuesq=0; valuesq < tlen; valuesq++)
+					this->set_coeff( coeffq, ( field->multiply( t->get_value(valuesq), field->power(valuesq,(tlen - i - 1)) ) ) );
+
+	}
+
+	uint64_t polynom64::get_length() {
+		return this->_length;
+	}
+
+	GF * polynom::get_field() {
+		return this->_field;
+	}
+	
 }
 
