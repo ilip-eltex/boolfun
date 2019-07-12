ca58df249518ba822d0c2c64244e63fc8396544b
Igor Lipovtsev <ilip.z@ya.ru>
2019-07-12 16:36:08 +0700

    Fixing

--- boolfun/test/main_test.cpp
+++ boolfun/test/main_test.cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
 #include <stdint.h>
 #include "bitslib.h"
-#include "sandbox.h"
+//#include "sandbox.h"
 
+class test {
+		
+		public:
+			bool result ();
+			std::string name ();
+			void set (std::string name, bool result);
+			
+		private:
+			bool _result;
+			std::string _name;
+};
+
+bool compare_vals (uint64_t a1, uint64_t a2, size_t size);
+void print_test (std::vector<test> &t, unsigned int index);
 
 using namespace bf;
 int main () {
@@ -41,3 +55,37 @@ int main () {
 	if (quit) return 1;
 	return 0;
 }
+
+/////////SOURCE
+
+	bool compare_vals (auto a1, auto a2, size_t size) {
+		char x[size] = static_cast<char*>(&a1);
+		char y[size] = static_cast<char*>(&a2);
+		return !(strcmp(a1,a2));
+	}
+	
+	
+	bool test::result () {
+		return this->_result;
+	}
+	
+	std::string test::name () {
+		return this->_name;
+	}
+	
+	void print_test (std::vector<test> &t, unsigned int index) {
+		std::cout 
+			<< '#'
+			<< index+1
+			<< ' '
+			<< t[index].name() 
+			<< ' ';
+		std::cout 
+			<< (t[index].result() ? "==PASSED==" : "==FAILED==")
+			<< std::endl;
+	}
+	
+	void test::set (std::string name, bool result) {
+		this->_name = name;
+		this->_result = result;
+	}
