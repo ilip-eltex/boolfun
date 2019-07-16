d051cbe0ecf8b768494088b2d8e7f61e9436b354
Igor Lipovtsev <ilip.z@ya.ru>
2019-07-16 13:32:28 +0700

    Update output writing

--- boolfun/test/main_test.cpp
+++ boolfun/test/main_test.cpp
@@ -30,49 +30,53 @@ int main () {
 	
 	// get_bit_32 
 	result32 = get_bit_32 ( static_cast<uint32_t>(0) , static_cast<unsigned int>(0) );
-	temp.set ("get_bit_32 (0,0)", result32 == 0);
+	temp.set ("get_bit_32(0,0) =0", result32 == 0);
 	tests.push_back(temp);
 	
 	// set_bit_32
 	result32 = 0;
 	set_bit_32 (result32,1,10);
-	temp.set("set_bit_32 (0,1,10)", result32 == 1024 );
+	temp.set("set_bit_32 (0,1,10) =1024", result32 == 1024 );
+	tests.push_back(temp);
+	
+	// set_bit_32 9[3],0 = 1  
+	result32 = 9;
+	set_bit_32(result32,0,3);
+	temp.set("set_bit_32 (9,0,3) =1", result32==1);
 	tests.push_back(temp);
 	
 	// set_bit1_32
 	result32 = 0;
 	set_bit1_32 (result32,10);
-	temp.set("set_bit1_32 (0,10)", result32 == 1024);
+	temp.set("set_bit1_32 (0,10) =1024", result32 == 1024);
 	tests.push_back(temp);
 	
 	//set_bit0_32
 	result32 = 1023;
 	set_bit0_32 (result32,9);
-	temp.set("set_bit0_32 (1023,9)", result32 == 511);
+	temp.set("set_bit0_32(1023,9) =511", result32 == 511);
 	tests.push_back(temp);
 	
 	//get_weight_32
 	result32 = get_weight_32 (15);
-	temp.set("get_weight_32 (15)", result32 == 4);
+	temp.set("get_weight_32(15) =4", result32 == 4);
 	tests.push_back(temp);
 	
 	// is_odd_32 
-	result32 = 9;
-	temp.set("is_odd_32 (10)", is_odd_32(result32) == true);
+	result32 = 10;
+	temp.set("is_odd_32(10) =TRUE", is_odd_32(result32) == true);
 	
 	// scalar_product 10 5 = 0
 	 result32 = scalar_product_32(10,5);
-	 temp.set("scalar_product_32 (10,5)", result32 == 0);
+	 temp.set("scalar_product_32(10,5) =0", result32 == 0);
 	 tests.push_back(temp);
 	 
 	// deg_32 (2048) 
 	temp_char = deg_32(2048);
-	temp.set("deg_32 (2048)", temp_char == 11);
+	temp.set("deg_32(2048) =11", temp_char == 11);
 	tests.push_back(temp); 
 	
-	for (int i=0; i<tests.size(); i++) 
-		if (not tests[i].result())
-			std::cout << 1 << std::endl;
+	
 	for (int i=0; i<tests.size(); i++) {
 		if ( not tests[i].result() ) 			
 			quit = true;
@@ -107,9 +111,15 @@ int main () {
 			<< ' '
 			<< t[index].name() 
 			<< ' ';
-		std::cout 
-			<< (t[index].result() ? "==PASSED==" : "==FAILED==")
-			<< std::endl;
+		std::string temp = t[index].name();
+		if (temp.size() < 27) {
+			for (int i=0; i < (27-temp.size()); i++)
+				std::cout << ' ';	
+		}
+		std::cout << "  "; 
+		if (t[index].result())	
+			std::cout << "~DONE\n";  
+		else std::cout << "~FAIL\n";
 	}
 	
 	void test::set (std::string name, bool result) {
