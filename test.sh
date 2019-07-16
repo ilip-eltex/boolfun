b3cbfd16e6807918614edb2ed7f53bf5e1c43b68
Igor Lipovtsev <ilip.z@ya.ru>
2019-07-16 11:13:00 +0700

    Fix test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" = "test" ]; then 
-  cd boolfun/test
-  g++ -std=c++11 main_test.cpp -o tests
-  ./tests
+  cd boolfun/test && g++ -std=c++11 main_test.cpp -o tests && ./tests
 fi
 #####
 if [ "$TRAVIS_BRANCH" = "master" ]; then
