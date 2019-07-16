693e1bafff2e89b2334423525f0b03ebbfb07862
comiam <maxim.bolshim@yandex.ru>
2019-07-16 15:44:22 +0700

    Some removes

--- boolfun/src/private_headers/GF_simple.h
+++ boolfun/src/private_headers/GF_simple.h
@@ -9,8 +9,7 @@ namespace bf
 		virtual bvect32 mod(bvect64);
 		virtual bvect32 get_generating_element();
 		virtual bvect64 sum(bvect64, bvect64);
-		virtual bvect64 save_x64_multiply(bvect64, bvect64);
-		virtual bvect64 div(GFSimple* field, bvect64 a, bvect64 b, int mode);
+		virtual bvect64 div(bvect64 a, bvect64 b, int mode);
 	public:
         virtual bvect32 sum(bvect32, bvect32);
 		virtual bvect32 multiply(bvect32, bvect32);
