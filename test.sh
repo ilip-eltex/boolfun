acc6fcb81035b9da19607ccfac1f8933d5ba962d
Igor Lipovtsev <ilip.z@ya.ru>
2019-07-16 12:02:20 +0700

    Continue fix test.sh.....

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #let x = 0;
-let result=`cd boolfun/test && g++ -std=c++11 main_test.cpp -o tests && ./tests`
+result=`cd boolfun/test && g++ -std=c++11 main_test.cpp -o tests && ./tests`
 if [ "$TRAVIS_BRANCH" = "test" ]; then 
 	exit $result
 fi
