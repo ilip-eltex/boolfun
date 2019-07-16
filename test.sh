a5f4f338091fc7ebb83afd010396d44ef34a8fe8
Igor Lipovtsev <ilip.z@ya.ru>
2019-07-16 11:53:29 +0700

    Continue fix test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 #let x = 0;
 #let result = 0;
 if [ "$TRAVIS_BRANCH" = "test" ]; then 
-	exit `boolfun/test && g++ -std=c++11 main_test.cpp -o tests && ./tests`
+	exit `cd boolfun/test && g++ -std=c++11 main_test.cpp -o tests && ./tests`
 fi
 #####
 if [ "$TRAVIS_BRANCH" = "master" ]; then
